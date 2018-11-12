e23f0b5b0ef2603652e92fe163c0e2a449b3468c
marim0ss <piyomaru.bunbun@gmail.com>
2018-11-12 17:10:23 +0900

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    User.create(users_params)
+  end
+
+
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(users_params)
+    redirect_to users_path
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to users_path
+  end
+
+
+  private
+  def users_params
+    params.require(:user).permit(:name, :age)
+  end
+end
